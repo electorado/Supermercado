de49ea316a5b47ce73b22e149137f68664c4533e
electorado <pamoncayo@gmail.com>
2026-02-11 23:34:52 +0100

    Añadiendo algunos métodos

--- src/supermercado/productos/Frescos.java
+++ src/supermercado/productos/Frescos.java
@@ -1,4 +1,5 @@
 package supermercado.productos;
+import java.security.spec.ECField;
 import java.util.ArrayList;
 
 public class Frescos {
@@ -18,9 +19,9 @@ public class Frescos {
 
     public String infoProducto(){
         return (
-                "Nombre:\t" + nombre + "\n" +
-                "Precio:\t" + precio + "\n" +
-                "Descuento:\t" + descuento + " %"
+                "Nombre:\t\t" + nombre + "\n" +
+                "Precio:\t\t" + precio + "€\n" +
+                "Descuento:\t" + descuento + "%"
                 );
     }
 
@@ -50,9 +51,34 @@ public class Frescos {
 
     public static void main(String[] args) {
         ArrayList<Frescos> carrito = new ArrayList<Frescos>();
-        Frescos pollo = new Frescos("pechuga de pollo", 5.60, 5);
+        Frescos pan = new Frescos("Hogaza integral", 3.60, 5);
+        carrito.add(pan);
+        System.out.println(pan.infoProducto());
+        System.out.println("Precio final:\t" + pan.getPrecioFinal() + "€");
+
+        Carne pollo = new Carne("pechuga de pollo", 7.89, 2.5, TipoCarne.ECO );
         carrito.add(pollo);
         System.out.println(pollo.infoProducto());
+        System.out.println("Precio final:\t" + pollo.getPrecioFinal() + "€");
+
+        FrutasyVegetales repollo = new FrutasyVegetales("Col rizada", 2.79, 0, TipoCultivo.KM_0);
+        carrito.add(pollo);
+        System.out.println(repollo.infoProducto());
+        System.out.println("Precio final:\t" + repollo.getPrecioFinal() + "€");
+
+        Lacteos yogurth = new Lacteos("pechuga de pollo", 4.25, 5, Grasa.ENTERO);
+        carrito.add(yogurth);
+        System.out.println(yogurth.infoProducto());
+        System.out.println("Precio final:\t" + yogurth.getPrecioFinal() + "€");
+
+        double total = 0;
+
+        for (int i = 0; i < carrito.size(); i++ ) {
+            total += carrito.get(i).getPrecioFinal();
+        }
+
+        System.out.println("Total a pagar:\t" + total +"€");
+
     }
 
 }
